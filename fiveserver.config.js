0498ee37723d60476fc1791e9e9f1349ac161f30
Gingka/Ginger Pepper <33764485+ExperiBass@users.noreply.github.com>
2024-05-26 13:02:11 -0400

    make fiverserver config

---
+++ fiveserver.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: 'site',
+    //host: '::1',
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    remoteLogs: true, // enable remoteLogs
+    remoteLogs: 'yellow', // enable remoteLogs and use the color yellow
+    injectCss: true, // enable injecting css
+    navigate: true, // enable auto-navigation
+}
