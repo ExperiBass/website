0452085b00b490e1d77826df449bca55c468bae4
Gingka/Ginger Pepper <33764485+ExperiBass@users.noreply.github.com>
2024-05-26 03:26:35 -0400

    savestate

--- build.js
+++ build.js
@@ -1,26 +1,41 @@
 const handlebars = require('handlebars')
-const {readFileSync, writeFileSync} = require('node:fs')
-const {join} = require('node:path')
+const { readFileSync, writeFileSync, readdirSync } = require('node:fs')
+const { join } = require('node:path')
 
+/// TODO
+/// compile views dir
 
 const LAYOUT_FILE = readFileSync(join(__dirname, './views/layout.handlebars'), 'utf-8')
 const LAYOUT = handlebars.compile(LAYOUT_FILE)
 const ctx = {
-    "index": {
-        tabtitle: "Ｔａｎｄｅｍ　ｖｕｌｐｅｓ　ｖｏｃｅｍ　ｓｕａｍ　ｒｅｐｅｒｉｅｔ．"
-    }
+    index: {
+        tabtitle: 'Ｔａｎｄｅｍ　ｖｕｌｐｅｓ　ｖｏｃｅｍ　ｓｕａｍ　ｒｅｐｅｒｉｅｔ．',
+        desc: 'Stinky Foxxo Does Things',
+    },
+    'cyberspace-independence': {
+        tabtitle: 'A Declaration of the Independence of Cyberspace',
+        desc:
+            '"A Declaration of the Independence of Cyberspace" is a widely distributed early paper ' +
+            'on the applicability (or lack thereof) of government to the rapidly growing Internet. ' +
+            'Commissioned for the pioneering Internet project 24 Hours in Cyberspace, ' +
+            'it was written by John Perry Barlow, a founder of the Electronic Frontier Foundation, ' +
+            'and published online on February 8, 1996, from Davos, Switzerland.',
+        author: 'John Perry Barlow',
+    },
 }
 function compileToHTML(path) {
     const template = readFileSync(join(__dirname, path), 'utf-8')
     const templateName = path.split('/')?.reverse()[0]?.split('.')[0]
-    const ctx = {
-        tabtitle: ,
-        page: `${templateName}.html`
+    const extra = {
+        page: `${templateName}.html`,
+        ...ctx[templateName],
     }
+    extra.author = extra.author || 'Gïng Pepper'
+    extra.stylesheets = extra.stylesheets || '<link rel="stylesheet" type="text/css" href="src/styles/main.css" />'
 
     const body = handlebars.compile(template)
 
-    return LAYOUT({body: body(ctx[templateName]), ...ctx[templateName]})
+    return LAYOUT({ body: body(...extra), ...extra })
 }
 
 console.log(compileToHTML('./views/index.handlebars'))
