2c25a7b3048c8e12e18b0df26e47d7d383d1e959
giing <33764485+ExperiBass@users.noreply.github.com>
2024-09-05 13:14:07 -0400

    laconic

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     root: 'site',
     //host: '::1',
-    ignore: [/\.handlebars$/g, /\.config.js$/g, /(build|galleries).js$/g],
+    ignore: ['src/views/', /\.config.js$/i, /(build|galleries).js$/i],
+    open: ['index.html'],
+    cache: true,
     highlight: true, // enable highlight feature
     injectBody: true, // enable instant update
     remoteLogs: true, // enable remoteLogs
     remoteLogs: 'yellow', // enable remoteLogs and use the color yellow
     injectCss: true, // enable injecting css
-    navigate: true, // enable auto-navigation
+    navigate: false, // enable auto-navigation
 }
